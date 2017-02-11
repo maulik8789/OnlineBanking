68c8540963669e02ab7e874b10ba9e09924ad31b
ashish vaghela <ashish@techservicesstudio.com>
2017-02-11 12:02:53 -0500

    -Created a MVC project

---
+++ VaghelaBank/VaghelaBank/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VaghelaBank.Startup))]
+namespace VaghelaBank
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
